805154c8cddd4e474a830e13ed5a6b776cd28453
Gerardo1209 <fematdelgado.github@gmail.com>
2023-09-16 14:07:08 -0600

    Terminando bienvendia y ajustes para la seleccion del usuario

--- app/src/main/java/com/example/viewlists/MainActivity.kt
+++ app/src/main/java/com/example/viewlists/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.example.viewlists
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ListView
+import android.widget.RadioGroup
 import android.widget.TextView
 import java.io.Serializable
+import android.content.SharedPreferences
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var TiendaAux = Tienda("Zegucom", "10,000.00")
@@ -31,22 +36,28 @@ class MainActivity : AppCompatActivity() {
         TiendaAux = Tienda("Zegucom", "4,329.00")
         TiendaAux2 = Tienda("Cyberpuerta", "4,309.00")
         TiendaAux3 = Tienda("Amazon", "8,500.00")
-        val producto3 = Producto("procesador AMD Ryzen 5 7600x",TiendaAux,TiendaAux2,TiendaAux3,"lo mismo que el g403 pero con PILAS",R.drawable.r7600x)
+        val producto3 = Producto("Procesador AMD Ryzen 5 7600x",TiendaAux,TiendaAux2,TiendaAux3,"lo mismo que el g403 pero con PILAS",R.drawable.r7600x)
         
         TiendaAux = Tienda("Zegucom", "10,000.00")
         TiendaAux2 = Tienda("Cyberpuerta", "6,739.00")
         TiendaAux3 = Tienda("Amazon", "10,500.00")
-        val producto4 = Producto("procesador AMD Ryzen 7 7800X3D",TiendaAux,TiendaAux2,TiendaAux3,"Menos de 100g para mover ese mouse como un diablo",R.drawable.r7800x3d)
+        val producto4 = Producto("Procesador AMD Ryzen 7 7800X3D",TiendaAux,TiendaAux2,TiendaAux3,"Menos de 100g para mover ese mouse como un diablo",R.drawable.r7800x3d)
         
         TiendaAux = Tienda("Zegucom", "10,000.00")
         TiendaAux2 = Tienda("Cyberpuerta", "6,739.00")
         TiendaAux3 = Tienda("Amazon", "10,500.00")
-        val producto5 = Producto("procesador AMD Ryzen 9 7950X",TiendaAux,TiendaAux2,TiendaAux3,"Menos de 100g para mover ese mouse como un diablo",R.drawable.r7950x)
+        val producto5 = Producto("Procesador AMD Ryzen 9 7950X",TiendaAux,TiendaAux2,TiendaAux3,"Menos de 100g para mover ese mouse como un diablo",R.drawable.r7950x)
 
         val listaProductos = listOf(producto,producto2,producto3, producto4, producto5)
 
         val adapter = ProductosAdapter(this, listaProductos)
         val  lista: ListView = findViewById(R.id.lista)
+        val config: Button = findViewById(R.id.configuracion)
+
+        config.setOnClickListener{ parent ->
+            val intent = Intent(this, ConfiguracionActivity::class.java)
+            startActivity(intent)
+        }
 
         lista.adapter = adapter
         lista.setOnItemClickListener{ parent, view, position, id ->
@@ -56,6 +67,54 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+}
+
+class ConfiguracionActivity : AppCompatActivity(){
+    private val PREFS_NAME = "rolUsuario"
+    private lateinit var sharedPreferences: SharedPreferences
+    private fun guardarSeleccion(seleccion: String) {
+        val editor = sharedPreferences.edit()
+        editor.putString("rol", seleccion)
+        editor.apply()
+    }
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_config)
+        sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val seleccionGuardada = sharedPreferences.getString("rol", null)
+
+        val opc = findViewById<RadioGroup>(R.id.opcRol)
+
+        if (seleccionGuardada != null) {
+            when (seleccionGuardada) {
+                "Estudiante" -> opc.check(R.id.opcEstudiante)
+                "Funcionario" -> opc.check(R.id.opcFuncionario)
+                "Obrero" -> opc.check(R.id.opcObrero)
+            }
+        }
+
+
+        opc.setOnCheckedChangeListener{
+            _, checkedId ->
+            when(checkedId){
+                R.id.opcEstudiante -> {
+                    guardarSeleccion("Estudiante")
+                }
+                R.id.opcFuncionario -> {
+                    guardarSeleccion("Funcionario")
+                }
+                R.id.opcObrero -> {
+                    guardarSeleccion("Obrero")
+                }
+            }
+        }
+
+        val regresar = findViewById<Button>(R.id.regresar)
+        regresar.setOnClickListener{
+            finish()
+        }
+
+    }
 }
 
 class Producto (val nombre:String, val Tienda1:Tienda,val Tienda2:Tienda,val Tienda3:Tienda,val descripcion:String, val imagen:Int): Serializable
