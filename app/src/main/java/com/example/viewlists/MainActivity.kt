84c97921836344c60e17d4b3f108e6f304a4d470
FcoMdz <al289539@edu.uaa.mx>
2023-09-16 15:29:55 -0600

    detalles finales

--- app/src/main/java/com/example/viewlists/MainActivity.kt
+++ app/src/main/java/com/example/viewlists/MainActivity.kt
@@ -26,29 +26,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var TiendaAux = Tienda("Zegucom", "10,000.00", "https://www.zegucom.com.mx/producto/procesadores/procesadores-intel-socket-1700-13a-gen/procesador-intel-core-i9-13900k-lga-1700-cache-36-mb-nucleos-24-hilos/Qlg4MDcxNTEzOTAwSw==")
-        var TiendaAux2 = Tienda("Cyberpuerta", "11,419.00", "https://www.cyberpuerta.mx/Computo-Hardware/Componentes/Procesadores/Procesadores-para-PC/Procesador-Intel-Core-i9-13900K-S-1700-24-Core-36MB-36MB-Smart-Cache-13va-Generacion-Raptor-Lake.html?srsltid=AfmBOoq4bViojZJU6yOJaRTbWV7ysSut_MUyTWJh2R9ynrXMnA0lgDH_uW4")
-        var TiendaAux3 = Tienda("Amazon", "12,166.00", "https://www.amazon.com.mx/Intel-BX8071513900K/dp/B0BCF54SR1/ref=sr_1_1?keywords=intel%2Bi9%2B13900k&qid=1694885585&sr=8-1&ufe=app_do%3Aamzn1.fos.a9e70178-7411-4f75-8d83-e6796a165895&th=1")
+        var TiendaAux = Tienda("Zegucom", "10,000.00", "https://www.zegucom.com.mx/producto/procesadores/procesadores-intel-socket-1700-13a-gen/procesador-intel-core-i9-13900k-lga-1700-cache-36-mb-nucleos-24-hilos/Qlg4MDcxNTEzOTAwSw==", R.drawable.zglogo)
+        var TiendaAux2 = Tienda("Cyberpuerta", "11,419.00", "https://www.cyberpuerta.mx/Computo-Hardware/Componentes/Procesadores/Procesadores-para-PC/Procesador-Intel-Core-i9-13900K-S-1700-24-Core-36MB-36MB-Smart-Cache-13va-Generacion-Raptor-Lake.html?srsltid=AfmBOoq4bViojZJU6yOJaRTbWV7ysSut_MUyTWJh2R9ynrXMnA0lgDH_uW4", R.drawable.cyberpuerta)
+        var TiendaAux3 = Tienda("Amazon", "12,166.00", "https://www.amazon.com.mx/Intel-BX8071513900K/dp/B0BCF54SR1/ref=sr_1_1?keywords=intel%2Bi9%2B13900k&qid=1694885585&sr=8-1&ufe=app_do%3Aamzn1.fos.a9e70178-7411-4f75-8d83-e6796a165895&th=1", R.drawable.amazonicon)
         var producto = Producto("Procesador Intel i9 13900k",TiendaAux,TiendaAux2,TiendaAux3,"El Procesador Intel Core i9-13900K es ideal para usuarios que requieren potencia informática extrema tanto en gaming como creación contenido profesional",R.drawable.i913900k)
 
-        TiendaAux = Tienda("Zegucom", "4,000.00","https://www.zegucom.com.mx/producto/procesadores/procesadores-intel-socket-1700-12a-gen/procesador-intel-core-i5-12600k-12va-gen-socket-lga1700-4-90ghz-6-4-n/Qlg4MDcxNTEyNjAwSw==")
-        TiendaAux2 = Tienda("Cyberpuerta", "6,219.00","https://www.cyberpuerta.mx/Computo-Hardware/Componentes/Procesadores/Procesadores-para-PC/Procesador-Intel-Core-i5-13600K-S-1700-3-50GHz-14-Core-24MB-Smart-Cache-13va-Generacion-Raptor-Lake.html?srsltid=AfmBOopTRIIHiYKXckBnyDfAnZkUUqzsYCZAqDZGzRUn1u6cI9dSbpmDNDM")
-        TiendaAux3 = Tienda("Amazon", "6,551.00","https://www.amazon.com.mx/Intel-i5-13600K-LGA1700-Desktop-Processor/dp/B0BG63WLG3")
+        TiendaAux = Tienda("Zegucom", "4,000.00","https://www.zegucom.com.mx/producto/procesadores/procesadores-intel-socket-1700-12a-gen/procesador-intel-core-i5-12600k-12va-gen-socket-lga1700-4-90ghz-6-4-n/Qlg4MDcxNTEyNjAwSw==", R.drawable.zglogo)
+        TiendaAux2 = Tienda("Cyberpuerta", "6,219.00","https://www.cyberpuerta.mx/Computo-Hardware/Componentes/Procesadores/Procesadores-para-PC/Procesador-Intel-Core-i5-13600K-S-1700-3-50GHz-14-Core-24MB-Smart-Cache-13va-Generacion-Raptor-Lake.html?srsltid=AfmBOopTRIIHiYKXckBnyDfAnZkUUqzsYCZAqDZGzRUn1u6cI9dSbpmDNDM", R.drawable.cyberpuerta)
+        TiendaAux3 = Tienda("Amazon", "6,551.00","https://www.amazon.com.mx/Intel-i5-13600K-LGA1700-Desktop-Processor/dp/B0BG63WLG3", R.drawable.amazonicon)
         val producto2 = Producto("Procesador intel i5 13600k",TiendaAux,TiendaAux2,TiendaAux3,"El procesador Intel Core i5-13600K 3.50GHz 14-Core BX8071513600K es un componente de alta gama para equipos de escritorio y gaming.",R.drawable.i513600k)
 
-        TiendaAux = Tienda("Zegucom", "4,329.00","https://www.zegucom.com.mx/producto/procesadores/procesadores-amd-socket-am5/procesador-amd-ryzen-5-7600x-socket-am5-4-7ghz-5-3ghz-38mb-cache-graf/MTAwLTEwMDAwMDU5M1dPRg==")
-        TiendaAux2 = Tienda("Cyberpuerta", "4,289.00","https://www.cyberpuerta.mx/Computo-Hardware/Componentes/Procesadores/Procesadores-para-PC/Procesador-AMD-Ryzen-5-7600X-S-AM5-4-70GHz-Six-Core-32MB-L3-Cache-no-Incluye-Disipador.html?srsltid=AfmBOooaNWk2E3w2Ku_5JAQAQN5j3bQLafFuiyYQfcm3F5hYt5QRn9Vi2PQ")
-        TiendaAux3 = Tienda("Amazon", "8,313.00","https://www.amazon.com.mx/Components-Procesador-100-000000593-Enfriador-Technology/dp/B0C28386RH")
+        TiendaAux = Tienda("Zegucom", "4,329.00","https://www.zegucom.com.mx/producto/procesadores/procesadores-amd-socket-am5/procesador-amd-ryzen-5-7600x-socket-am5-4-7ghz-5-3ghz-38mb-cache-graf/MTAwLTEwMDAwMDU5M1dPRg==", R.drawable.zglogo)
+        TiendaAux2 = Tienda("Cyberpuerta", "4,289.00","https://www.cyberpuerta.mx/Computo-Hardware/Componentes/Procesadores/Procesadores-para-PC/Procesador-AMD-Ryzen-5-7600X-S-AM5-4-70GHz-Six-Core-32MB-L3-Cache-no-Incluye-Disipador.html?srsltid=AfmBOooaNWk2E3w2Ku_5JAQAQN5j3bQLafFuiyYQfcm3F5hYt5QRn9Vi2PQ", R.drawable.cyberpuerta)
+        TiendaAux3 = Tienda("Amazon", "8,313.00","https://www.amazon.com.mx/Components-Procesador-100-000000593-Enfriador-Technology/dp/B0C28386RH", R.drawable.amazonicon)
         val producto3 = Producto("Procesador AMD Ryzen 5 7600x",TiendaAux,TiendaAux2,TiendaAux3,"El procesador AMD Ryzen 5 7600X S-AM5 es un componente de alta gama diseñado para ofrecer un rendimiento excepcional en aplicaciones exigentes",R.drawable.r7600x)
 
-        TiendaAux = Tienda("Zegucom", "8,371.00","https://www.zegucom.com.mx/producto/procesadores/procesadores-amd-socket-am5/procesador-amd-ryzen-7-7800x3d-amd-ryzen-7-skt-am5-5nm-4-2-ghz-8-core/MTAwLTEwMDAwMDkxMFdPRg==")
-        TiendaAux2 = Tienda("Cyberpuerta", "4,289.00","https://www.cyberpuerta.mx/Computo-Hardware/Componentes/Procesadores/Procesadores-para-PC/Procesador-AMD-Ryzen-7-7800X3D-S-AM5-4-20GHz-8-Core-96MB-L3-Cache-No-Incluye-Disipador.html?srsltid=AfmBOopegFRf7hYX1d_3HpqgABLZuX-5TA1qO9gUOJLwE-eBa01uO-ZeVZw")
-        TiendaAux3 = Tienda("Amazon", "7,532.00","https://www.amazon.com.mx/AMD-7800X3D-Radeon-Graphics-100-100000910WOF/dp/B0BTZB7F88")
+        TiendaAux = Tienda("Zegucom", "8,371.00","https://www.zegucom.com.mx/producto/procesadores/procesadores-amd-socket-am5/procesador-amd-ryzen-7-7800x3d-amd-ryzen-7-skt-am5-5nm-4-2-ghz-8-core/MTAwLTEwMDAwMDkxMFdPRg==", R.drawable.zglogo)
+        TiendaAux2 = Tienda("Cyberpuerta", "4,289.00","https://www.cyberpuerta.mx/Computo-Hardware/Componentes/Procesadores/Procesadores-para-PC/Procesador-AMD-Ryzen-7-7800X3D-S-AM5-4-20GHz-8-Core-96MB-L3-Cache-No-Incluye-Disipador.html?srsltid=AfmBOopegFRf7hYX1d_3HpqgABLZuX-5TA1qO9gUOJLwE-eBa01uO-ZeVZw", R.drawable.cyberpuerta)
+        TiendaAux3 = Tienda("Amazon", "7,532.00","https://www.amazon.com.mx/AMD-7800X3D-Radeon-Graphics-100-100000910WOF/dp/B0BTZB7F88", R.drawable.amazonicon)
         val producto4 = Producto("Procesador AMD Ryzen 7 7800X3D",TiendaAux,TiendaAux2,TiendaAux3,"El procesador AMD Ryzen 7 7800X3D es un componente de la familia de procesadores AMD Ryzen™ 7, diseñado para su uso en equipos de escritorio.",R.drawable.r7800x3d)
 
-        TiendaAux = Tienda("Zegucom", "10,000.00","https://www.zegucom.com.mx/producto/procesadores/procesadores-amd-socket-am5/procesador-amd-ryzen-9-7950x-socket-am5-4-5-ghz-5-7-ghz-max-boost-80m/MTAwLTEwMDAwMDUxNFdPRg==")
-        TiendaAux2 = Tienda("Cyberpuerta", "10,279.00","https://www.cyberpuerta.mx/Computo-Hardware/Componentes/Procesadores/Procesadores-para-PC/Procesador-AMD-Ryzen-9-7950X-Radeon-Graphics-S-AM5-4-50GHz-16-Core-64MB-Cache-No-Incluye-Disipador.html?srsltid=AfmBOoo1xPSx0UW4UHqmfM5t-6d1qDLW_llKTC-uEZSOC1GwRpFL6Xppc9Q")
-        TiendaAux3 = Tienda("Amazon", "11,664..00","https://www.amazon.com.mx/AMD-RyzenTM-9-7950X-Procesador/dp/B0BBHD5D8Y")
+        TiendaAux = Tienda("Zegucom", "10,000.00","https://www.zegucom.com.mx/producto/procesadores/procesadores-amd-socket-am5/procesador-amd-ryzen-9-7950x-socket-am5-4-5-ghz-5-7-ghz-max-boost-80m/MTAwLTEwMDAwMDUxNFdPRg==", R.drawable.zglogo)
+        TiendaAux2 = Tienda("Cyberpuerta", "10,279.00","https://www.cyberpuerta.mx/Computo-Hardware/Componentes/Procesadores/Procesadores-para-PC/Procesador-AMD-Ryzen-9-7950X-Radeon-Graphics-S-AM5-4-50GHz-16-Core-64MB-Cache-No-Incluye-Disipador.html?srsltid=AfmBOoo1xPSx0UW4UHqmfM5t-6d1qDLW_llKTC-uEZSOC1GwRpFL6Xppc9Q", R.drawable.cyberpuerta)
+        TiendaAux3 = Tienda("Amazon", "11,664.00","https://www.amazon.com.mx/AMD-RyzenTM-9-7950X-Procesador/dp/B0BBHD5D8Y", R.drawable.amazonicon)
         val producto5 = Producto("Procesador AMD Ryzen 9 7950X",TiendaAux,TiendaAux2,TiendaAux3,"El procesador AMD Ryzen 9 7950X Radeon Graphics es un componente de alta gama diseñado para ofrecer un rendimiento excepcional en aplicaciones exigentes",R.drawable.r7950x)
 
         val listaProductos = listOf(producto,producto2,producto3, producto4, producto5)
@@ -127,13 +127,22 @@ class ProductosAdapter (private val mContext: Context, private val listaProducto
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val layout = LayoutInflater.from(mContext).inflate(R.layout.item_producto, parent, false)
         val nombreTextView = layout.findViewById<TextView>(R.id.nombre)
-        val precioTextView = layout.findViewById<TextView>(R.id.precio)
         val imageView = layout.findViewById<ImageView>(R.id.imageView)
+        val TimageView1 = layout.findViewById<ImageView>(R.id.TimageView1)
+        val TimageView2 = layout.findViewById<ImageView>(R.id.TimageView2)
+        val TimageView3 = layout.findViewById<ImageView>(R.id.TimageView3)
+        val Tprecio1 =layout.findViewById<TextView>(R.id.Tprecio1)
+        val Tprecio2 =layout.findViewById<TextView>(R.id.Tprecio2)
+        val Tprecio3 =layout.findViewById<TextView>(R.id.Tprecio3)
         val producto = listaProductos[position]
-        nombreTextView.text = producto.nombre
-        precioTextView.text = "clic para conocer los precios disponibles"
         imageView.setImageResource(producto.imagen)
-
+        nombreTextView.text = producto.nombre
+        TimageView1.setImageResource(listaProductos[position].Tienda1.Timg)
+        Tprecio1.text = listaProductos[position].Tienda1.Tprecio
+        TimageView2.setImageResource(listaProductos[position].Tienda2.Timg)
+        Tprecio2.text = listaProductos[position].Tienda2.Tprecio
+        TimageView3.setImageResource(listaProductos[position].Tienda3.Timg)
+        Tprecio3.text = listaProductos[position].Tienda3.Tprecio
         return layout
     }
 
@@ -173,7 +182,7 @@ class ProductoActivity : AppCompatActivity(){
     }
 }
 
-class Tienda (val Tnombre:String, val Tprecio:String, val url: String): Serializable
+class Tienda (val Tnombre:String, val Tprecio:String, val url: String,val Timg:Int): Serializable
 
 class TiendaAdapter (private val mContext: Context, private val listaTienda: List<Tienda>) :
     ArrayAdapter <Tienda>(mContext, 0 , listaTienda) {
